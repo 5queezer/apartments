7b095b48c32f07affc3ae8060dccfe2cf96c8ebe
Christian Pojoni <dev@pojoni.at>
2019-12-30 18:57:50 +0100

    layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,30 @@
 <template>
-  <div>
-    <logo />
-    <navbar />
-  </div>
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <!-- logo /-->
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <!-- navbar /-->
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col />
+      <b-col />
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import Navbar from '~/components/Navbar.vue'
+// import Logo from '~/components/Logo.vue'
+// import Navbar from '~/components/Navbar.vue'
+
 export default {
   components: {
-    Logo,
-    Navbar
+    // Logo
+    // Navbar
   }
 }
 </script>
-
-<style>
-</style>
