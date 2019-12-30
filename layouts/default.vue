092de46425c68a6bf3e0b23fb417641f559fb5fb
Christian Pojoni <dev@pojoni.at>
2019-12-30 03:17:23 +0100

    styling

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-cloak>
     <nuxt />
   </div>
 </template>
@@ -51,5 +51,13 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+[v-cloak] {
+  display: none;
+}
+
+.navbar {
+  list-style-type: none;
 }
 </style>
