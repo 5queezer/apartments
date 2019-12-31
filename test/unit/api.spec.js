c095694e1fdb8ee221c27d1fd262c666f35f376f
Christian Pojoni <dev@pojoni.at>
2019-12-31 19:41:51 +0100

    refactoring test

--- test/unit/api.spec.js
+++ test/unit/api.spec.js
@@ -2,21 +2,14 @@ import flushPromises from 'flush-promises'
 import api from '@/assets/api.faker'
 
 describe('Api', () => {
-  it('fetches the data', async () => {
-    // act
-    const result = await api.getApartments()
-    await flushPromises()
+  const result = await api.getApartments()
+  await flushPromises()
 
-    // assert
+  it('fetches the data', async () => {
     expect(result.length).toEqual(10)
   })
 
   it('matches the snapshot', async () => {
-    // act
-    const result = await api.getApartments()
-    await flushPromises()
-
-    // assert
     expect(result).toMatchSnapshot()
   })
 })
