ec7a90d639b9ccb343a6d4c196a96a4c128898a2
Christian Pojoni <dev@pojoni.at>
2019-12-31 20:55:19 +0100

    store set mutation

---
+++ store/mutations.js
@@ -0,0 +1,15 @@
+export default {
+  SET_APARTMENT (state, apartment) {
+    state.apartments.push({
+      title: apartment.title,
+      price: apartment.price,
+      bedrooms: apartment.bedrooms,
+      bathrooms: apartment.bathrooms,
+      picture: apartment.picture,
+      geo: {
+        lat: apartment.geo.lat,
+        long: apartment.get.long
+      }
+    })
+  }
+}
