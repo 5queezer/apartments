9014b27d454889056b5556eb36584e82eee14e80
Christian Pojoni <dev@pojoni.at>
2020-01-01 22:33:49 +0100

    scroll overflow

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container fluid>
+  <b-container id="main" fluid>
     <b-row class="mb-3">
       <b-col>
         <logo />
@@ -10,29 +10,62 @@
         <navbar />
       </b-col>
     </b-row>
-    <b-row>
+    <b-row id="stretch">
       <b-col id="listview">
         <list :apartments="list" />
       </b-col>
-      <b-col />
+      <b-col>
+        <maps :locations="locations" />
+      </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
+import Vuex, { mapGetters, mapActions } from 'vuex'
+import Vue from 'vue'
 import Logo from '~/components/Logo.vue'
 import Navbar from '~/components/Navbar.vue'
 import List from '~/components/List.vue'
+import Maps from '~/components/Maps.vue'
+
+Vue.use(Vuex)
 
 export default {
   components: {
     Logo,
     Navbar,
-    List
+    List,
+    Maps
   },
   computed: {
-    ...mapGetters('apartments', [ 'list' ])
+    ...mapGetters('apartments', [ 'list' ]),
+    locations () {
+      return [
+        {
+          lat: 44.933076,
+          lng: 15.629058
+        },
+        {
+          lat: 45.815,
+          lng: 15.9819
+        },
+        {
+          lat: 45.12,
+          lng: 16.21
+        }
+      ]
+
+      // const locations = []
+      // for (const apartment of this.list) {
+      //   locations.push({
+      //     id: parseInt(apartment.id),
+      //     lat: parseFloat(apartment.address.lat),
+      //     lng: parseFloat(apartment.address.long)
+      //   })
+      // }
+      // return locations
+    }
   },
   mounted () {
     this.fetchAll()
@@ -44,7 +77,16 @@ export default {
 </script>
 
 <style>
+#main {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+#stretch {
+  overflow: hidden;
+}
 #listview {
-
+  height: 100%;
+  overflow-y: scroll;
 }
 </style>
