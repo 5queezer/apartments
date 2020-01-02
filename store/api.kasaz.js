3fdb2be36182a256bae87a8f992b7ba5c986e332
Christian Pojoni <dev@pojoni.at>
2020-01-02 16:08:38 +0100

    reformat api input

--- store/api.kasaz.js
+++ store/api.kasaz.js
@@ -1,5 +1,38 @@
-export default {
-  getApartments () {
-    return []
+import { setup } from 'axios-cache-adapter'
+
+const api = setup({
+  baseURL: 'https://www.kasaz.com/api/v1',
+  cache: {
+    maxAge: 15 * 60 * 1000
   }
+})
+
+function format (d) {
+  const item = {}
+  item.id = d.id
+  item.title = d.t
+  item.price = d.p
+  item.sqm = d.s
+  item.bedrooms = d.r
+  item.bathrooms = d.b
+  item.picture = d.i.map(element => element.imageUrl)
+  item.address = {
+    city: d.cn,
+    street: d.dn,
+    lat: d.l,
+    long: d.g
+  }
+  return item
+}
+
+function getApartments (params) {
+  return api.get('search/update_map_results', { params })
+    .then(function (response) {
+      const data = response.data.markers.map(format)
+      return data
+    })
+}
+
+export default {
+  getApartments
 }
--- test/unit/store/api_kasaz.spec.js
+++ test/unit/store/api_kasaz.spec.js
@@ -4,4 +4,18 @@ describe('Api', () => {
   it('getApartments function exists', () => {
     expect(typeof api.getApartments).toEqual('function')
   })
+
+  it('can fetch', async () => {
+    const results = await api.getApartments()
+    expect(results).toEqual([])
+  })
+
+  it('can fetch with parameters', async () => {
+    const params = {
+      'location[city]': 'Barcelona',
+      'filters[price][min]': 1.5e6
+    }
+    const results = await api.getApartments(params)
+    expect(results.length > 1).toBeTruthy()
+  })
 })
