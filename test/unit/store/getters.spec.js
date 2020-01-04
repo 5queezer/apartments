d86dbc62412841717fcbc0c1410aecb1d5f24a11
Christian Pojoni <dev@pojoni.at>
2020-01-04 14:58:52 +0100

    refactoring test for getters

--- test/unit/store/getters.spec.js
+++ test/unit/store/getters.spec.js
@@ -3,25 +3,27 @@ import { getters, state as initialState } from '@/store/apartments'
 
 describe('Getters', () => {
   const deepCopy = other => JSON.parse(JSON.stringify(other))
-  let state
+  let state, copy
 
   beforeEach(() => {
     state = deepCopy(initialState())
+    copy = deepCopy(apartmentFixture)
   })
 
   it('gets Apartments', () => {
     // act
-    state.list = [apartmentFixture]
+    state.list = [copy]
     const apartments = getters.list(state)
     // assert
     expect(apartments.length).toBe(1)
   })
 
   it('current apartment object', () => {
-    state.list = [Object.assign(apartmentFixture, { id: 10 })]
+    copy.id = 10
+    state.list = [copy]
     state.id = 10
-    const apartment = getters.current(state)
+    const current = getters.current(state)
     // assert
-    expect(apartment).toEqual(apartmentFixture)
+    expect(current).toEqual(copy)
   })
 })
